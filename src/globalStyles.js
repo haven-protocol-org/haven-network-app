2cd79028cef46b1fcf8edd0c81991ecd61ede10a
Pierre Lafitte <pierrelafitte@protonmail.com>
2020-07-01 12:05:42 -0400

    Setup styles

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -5,11 +5,10 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
     font-size: 16px;
+    font-family: 'Inter', sans-serif;
+    font-family: 'Playfair Display', serif;
     text-rendering: optimizeLegibility;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     font-feature-settings: 'zero' 1
-    font-family: 'Inter', sans-serif;
-    font-family: 'Playfair Display', serif;
-  
   }`;
