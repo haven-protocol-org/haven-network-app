69d7a347e9c1ed41c3c558a8479485d41891cc90
Pierre Lafitte <pierrelafitte@protonmail.com>
2020-11-09 15:38:51 -0400

    quick format change

--- src/src/components/tables/marketData/index.js
+++ src/src/components/tables/marketData/index.js
@@ -19,10 +19,6 @@ class MarketData extends Component {
     var formatter = new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
-
-      // These options are needed to round to whole numbers if that's what you want.
-      //minimumFractionDigits: 0,
-      //maximumFractionDigits: 0,
     });
 
     return (
