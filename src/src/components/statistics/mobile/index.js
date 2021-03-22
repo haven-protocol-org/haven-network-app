73cc100a60eb3d019fd8271c3d8bf2167cb203c2
Pierre Lafitte <pierrelafitte@protonmail.com>
2021-03-22 12:26:12 -0400

    Duplicate component out of pure laziness.

---
+++ src/src/components/statistics/mobile/index.js
@@ -0,0 +1,38 @@
+// Library Imports
+import React from "react";
+
+// Relative Imports
+import { Data, Grid, Value, Label, Statistic } from "./styles";
+
+const Statistics = ({ xUSD, xhv_circ, xusd_circ, network_value }) => {
+  return (
+    <Grid>
+      <Statistic>
+        <Data>
+          <Value>${xUSD}</Value>
+          <Label>Haven 24h MA Price</Label>
+        </Data>
+      </Statistic>
+      <Statistic>
+        <Data>
+          <Value>{xhv_circ}</Value>
+          <Label>XHV Circulating Supply</Label>
+        </Data>
+      </Statistic>
+      <Statistic>
+        <Data>
+          <Value>{xusd_circ}</Value>
+          <Label>xUSD Circulating Supply</Label>
+        </Data>
+      </Statistic>
+      <Statistic>
+        <Data>
+          <Value>{network_value}</Value>
+          <Label>Total Network Value</Label>
+        </Data>
+      </Statistic>
+    </Grid>
+  );
+};
+
+export default Statistics;
